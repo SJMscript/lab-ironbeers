d09381d2de510391bdb6fa4b0fd53245a4c353e4
Santiago Massa <sjm_95@hotmail.com>
2023-05-03 18:37:57 +0200

    santi push

--- app.js
+++ app.js
@@ -14,12 +14,50 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get("/beers", (req, res) =>{
+punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    console.log('Beers from the database: ', beersFromApi)
+    res.render("beers",{
+      allBeers: beersFromApi
+    })
+    })
+  .catch(error => console.log(error));
+
+});
+
+/*
+app.get("/beers", async function (req, res) {
+  try {
+    const beersFromApi = await punkAPI.getBeers();
+    res.render("beers", { beers: beersFromApi});
+  } catch ( error ) {
+    console.log(error)
+  }
+})
+*/
+
+app.get("/random-beer", (req, res) =>{
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    console.log("Respuesta", responseFromAPI)
+    res.render("random-beer", {
+      randomOne: responseFromAPI
+    })
+  })
+  .catch(error => console.log(error));
+
+})
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
